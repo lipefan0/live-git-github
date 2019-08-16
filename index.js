57deb3a3234c366720b6ba010d5408e062160020
lipefan0 <51931392+lipefan0@users.noreply.github.com>
2019-08-16 13:52:21 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const App = express();
 
 App.get('/teste', (res, req) => {
-  return res.json({ hello: 'Wolrd' })
+  return res.json({ hello: 'World 2' })
 });
 
 App.listen(3333);
